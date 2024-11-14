28c84203992c4449d0becd3aaa2c334e95e1fff7
amossohotra0 <amossohotra0@gmail.com>
2024-11-14 10:00:00 -0500

    Home page for the app

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import PropertiesList from "@/components/home/PropertiesList";
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <PropertiesList />
+    </>
+  );
+}
